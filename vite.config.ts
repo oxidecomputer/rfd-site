a899da0dc545f5bd771fc15794ddcf56f43e5349
Benjamin Leonard <hello@benleonard.co.uk>
2025-01-24 18:27:19 +0000

    Try `noExternal` code mirror basic setup

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,6 @@ export default defineConfig({
     port: 3000,
   },
   ssr: {
-    noExternal: ['@uiw/react-codemirror'],
+    noExternal: ['@uiw/react-codemirror', '@uiw/codemirror-extensions-basic-setup'],
   },
 })
