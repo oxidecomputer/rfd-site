f2917d5bdb181c19446da878504ca2925178339e
Benjamin Leonard <benji@oxide.computer>
2025-10-13 12:34:28 +0100

    Fix focus

--- app/components/TextInput.tsx
+++ app/components/TextInput.tsx
@@ -27,7 +27,7 @@ export const TextInput = forwardRef<HTMLInputElement, TextInputBaseProps>(
           ref={ref}
           type={type}
           className={cn(
-            `text-sans-md text-default bg-default placeholder:text-quaternary disabled:text-tertiary disabled:bg-disabled w-full rounded border-none px-3 py-[0.6875rem] !outline-offset-1 focus:outline-none disabled:cursor-not-allowed`,
+            `text-sans-md text-default bg-default placeholder:text-quaternary disabled:text-tertiary disabled:bg-disabled focus:outline-accent-secondary w-full rounded border-none px-3 py-[0.6875rem] !outline-offset-1 disabled:cursor-not-allowed`,
             disabled && 'text-disabled bg-disabled',
           )}
           disabled={disabled}
